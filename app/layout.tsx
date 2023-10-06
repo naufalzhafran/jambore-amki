725c5b96fdbf0c8ce03f20e6d6868b9e883b7bfc
Naufal Zhafran Latif <latifnaufal7@gmail.com>
2023-10-06 07:52:59 +0700

    feat: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,17 @@ export default function RootLayout({
   return (
     <html lang="id">
       <body className={inter.className}>
+        {/* Google Analytics */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-QJNJ1EKX3D" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-QJNJ1EKX3D');
+        `}
+        </Script>
         {children}
         <Toaster />
       </body>
